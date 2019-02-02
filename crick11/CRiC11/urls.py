f724ca824f9b4c1b208ffed279358f299308b097
User <akshayregu7@gmail.com>
2019-02-02 14:25:03 +0530

    started

--- crick11/CRiC11/urls.py
+++ crick11/CRiC11/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
+
+app_name = 'CRiC11'
 urlpatterns = [
-    path('Player/',views.IndexView.as_view,name="Playerlist"),
-    path('cric11/',views.DetailView.as_view,name="PlayerDetail"),
+    path('Player/', views.IndexView.as_view(), name="Playerlist"),
+    path('cric11/<int:pk>/', views.DetailView1.as_view(), name="PlayerDetail"),
+
 ]
