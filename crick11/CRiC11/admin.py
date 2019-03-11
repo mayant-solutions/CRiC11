88b969730a0cb23abd84be1bcabfe497a8e2b159
mayant-solutions <44162740+mayant-solutions@users.noreply.github.com>
2019-03-11 15:07:05 +0530

    Update admin.py

--- crick11/CRiC11/admin.py
+++ crick11/CRiC11/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Player, Bat, Bowl, BallType, BatType,Matches,ScoreCard, Teams
+from .models import Player, Bat, Bowl, BallType,ScoreCard
 
 admin.site.register(Player)
 admin.site.register(Bat)
 admin.site.register(Bowl)
 admin.site.register(BallType)
-admin.site.register(BatType)
-admin.site.register(Matches)
+
 admin.site.register(ScoreCard)
-admin.site.register(Teams)
